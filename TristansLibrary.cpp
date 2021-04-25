05655dd9b01e7915e4ca3bff34b7d587932b33a3
Tristan367 <71802741+Tristan367@users.noreply.github.com>
2021-04-24 18:23:40 -0700

    Add files via upload
    Added a bunch of stuff

--- TristansLibrary.cpp
+++ TristansLibrary.cpp
@@ -1,5 +1,6 @@
 #include "TristansLibrary.h"
 
+
 unsigned int randumb32(void) { 
     static unsigned int z1 = 3, z2 = 7, z3 = 13, z4 = 19;
     z1 = z1 * 9 + 1; // equation gets all 4,294,967,296 numbers
@@ -47,15 +48,192 @@ unsigned int lfsr113_Bits(void)
     return (z1 ^ z2 ^ z3 ^ z4);
 }
 
+Randumb::Randumb() {
+    time_t t;
+    time(&t);
+    s = t;
+}
 Randumb::Randumb(unsigned long long seed) {
     s = seed;
 };
 unsigned long long Randumb::Next() {
-    s = s * 9 + 1;
+    s = (s * 9 + 1); // modulus this by an odd number, an even number mod leads to alternating between odd and even numbers, therefore you also never get the same number twice or more in a row
     return s;
+}
+int Randumb::NextRanged(int min, int max) {
+    unsigned int width = max - min;
+    int offset = max - width;
+    return (Next() % width) + offset;
+}
+float Randumb::NextFloat0To1() {
+    int resolution = 1000000001;
+    if (s < 111111111) { // to prevent several low numbers in a row everytime the seed is below the resolution / 9
+        s *= 12345;
+    }
+    return (float)(Next() % resolution) / resolution;
+}
+float Randumb::NextRangedFloat(float min, float max) {
+    float r = NextFloat0To1();
+    float width = max - min;
+    float offset = max - width;
+    return (width * r) + offset;
+}
+bool Randumb::CoinFlip() {
+    return (Next() % 100) >= 50;
 }
 
 
+Float3::Float3() {
+    x = 0;
+    y = 0;
+    z = 0;
+};
+Float3::Float3(float xC, float yC, float zC) {
+    x = xC;
+    y = yC;
+    z = zC;
+}
+Float3 Float3::operator*(float f) {
+    Float3 ret(x, y, z);
+    ret.x *= f;
+    ret.y *= f;
+    ret.z *= f;
+    return ret;
+}
+Float3 Float3::operator+(Float3 v) {
+    Float3 ret(x, y, z);
+    ret.x += v.x;
+    ret.y += v.y;
+    ret.z += v.z;
+    return ret;
+}
+float Float3::DotProduct(Float3 v) {
+    return (x * v.x) + (y * v.y) + (z * v.z);
+}
 
 
+Plane::Plane() {
+    a = Float3();
+    b = Float3();
+    c = Float3();
+}
+Plane::Plane(Float3 aC, Float3 bC, Float3 cC) {
+    a = aC;
+    b = bC;
+    c = cC;
+}
+Plane::Plane(Float3 aC, Float3 bC, Float3 cC, Float3 norm) {
+    a = aC;
+    b = bC;
+    c = cC;
+    normal = norm;
+}
+// TODO: this function
+bool Plane::PointIsOnPositiveSideOfPlane(Float3 p) {
 
+
+}
+
+
+Transform::Transform() {
+    right = Float3();
+    up = Float3();
+    forward = Float3();
+    position = Float3();
+}
+Transform::Transform(Float3 r, Float3 u, Float3 f) {
+    right = r;
+    up = u;
+    forward = f;
+    position = Float3();
+}
+Transform::Transform(Float3 r, Float3 u, Float3 f, Float3 p) {
+    right = r;
+    up = u;
+    forward = f;
+    position = p;
+}
+Transform::Transform(Float3 p) {
+    right = Float3();
+    up = Float3();
+    forward = Float3();
+    position = p;
+}
+Float3 Transform::LocalPointToWorldSpace(Float3 p) {
+    Float3 u1 = right * p.x;
+    Float3 u2 = up * p.y;
+    Float3 u3 = forward * p.z;
+    return u1 + u2 + u3;
+}
+
+
+bool AABBIsInFrustumPlanes(Float3*corners, Plane *planes)
+{
+    bool inFrustum;
+    for (int i = 0; i < 6; i++)
+    {
+        inFrustum = false;
+        for (int j = 0; j < 8; j++)
+        {
+            if (planes[i].PointIsOnPositiveSideOfPlane(corners[j]))
+            {
+                inFrustum = true;
+            }
+        }
+        if (!inFrustum)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+// miscellaneous functions
+
+void addIntNoOverflowOrUnderflow(int& n, int add) {
+    if (add >= 0) {
+        if (n + add < n) {
+            n = sCap;
+            return;
+        }
+    }
+    else {
+        if (n + add > n) {
+            n = sCapMin;
+            return;
+        }
+    }
+    n = n + add;
+}
+void addUnsignedIntNoOverflowOrUnderflow(unsigned int& n, int add) {
+
+    if (add >= 0) {
+        if (n + add < n) {
+            n = uCap;
+            return;
+        }
+    }
+    else {
+        if (n + add > n) {
+            n = 0;
+            return;
+        }
+    }
+    n = n + add;
+}
+
+float averageInt(int* ints, int size) {
+    int avg = 0;
+    for (int i = 0; i < size; i++) {
+        avg += ints[i];
+    }
+    return (float)avg / size;
+}
+float averageFloat(float* floats, int size) {
+    float avg = 0;
+    for (int i = 0; i < size; i++) {
+        avg += floats[i];
+    }
+    return avg / size;
+}
--- TristansLibrary.h
+++ TristansLibrary.h
@@ -1,5 +1,22 @@
 #pragma once
-#include <iostream>
+#include <ctime>
+
+const unsigned int uCap = ~0;
+const int sCap = uCap >> 1;
+const int sCapMin = -sCap - 1;
+
+const unsigned short uCap16 = ~0;
+const short sCap16 = uCap16 >> 1;
+const short sCapMin16 = -sCap16 - 1;
+
+const unsigned char uCap8 = ~0;
+const char sCap8 = uCap8 >> 1;
+const char sCapMin8 = -sCap8 - 1;
+
+const double pi = 3.141592653589793238;
+const double sqrRt2 = 1.414213562373095048;
+const double sqrRt3 = 1.732050807568877293;
+const double goldenRatio = 1.618033988749894848;
 
 unsigned int randumb32(void);
 unsigned short randumb16(void);
@@ -10,9 +27,54 @@ class Randumb
 private:
     unsigned long long s;
 public:
+    Randumb();
     Randumb(unsigned long long seed);
+
     unsigned long long Next();
+    int NextRanged(int min, int max);
+    float NextFloat0To1();
+    float NextRangedFloat(float min, float max);
+    bool CoinFlip();
 };
 
+class Float3
+{
+public:
+    float x, y, z;
+    Float3();
+    Float3(float x, float y, float z);
+
+    Float3 operator*(float f);
+    Float3 operator+(Float3 v);
+    float DotProduct(Float3 v);
+};
+
+class Transform 
+{
+public:
+    Float3 right, up, forward, position;
+    Transform();
+    Transform(Float3 right, Float3 up, Float3 forward);
+    Transform(Float3 right, Float3 up, Float3 forward, Float3 position);
+    Transform(Float3 position);
+
+    Float3 LocalPointToWorldSpace(Float3 p);
+};
+
+class Plane {
+public:
+    Float3 a, b, c, normal;
+    Plane();
+    Plane(Float3 a, Float3 b, Float3 c);
+    Plane(Float3 a, Float3 b, Float3 c, Float3 norm);
+
+    bool PointIsOnPositiveSideOfPlane(Float3 point);
+};
+
+void addIntNoOverflowOrUnderflow(int& n, int add);
+void addUnsignedIntNoOverflowOrUnderflow(unsigned int& n, int add);
+
+float averageInt(int* ints, int size);
+float averageFloat(float* floats, int size);
 
 
